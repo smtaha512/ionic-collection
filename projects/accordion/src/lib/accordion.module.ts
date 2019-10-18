21d1c99f7622b81403e74d0619852912069fe7f8
Syed Muhammad Taha <taha@loopsdigital.com>
2019-10-18 14:26:35 +0500

    feat(accordion): add library for accordion

---
+++ projects/accordion/src/lib/accordion.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { AccordionComponent } from './accordion.component';
+
+
+
+@NgModule({
+  declarations: [AccordionComponent],
+  imports: [
+  ],
+  exports: [AccordionComponent]
+})
+export class AccordionModule { }
